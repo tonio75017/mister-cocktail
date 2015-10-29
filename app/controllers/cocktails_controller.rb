8884bc6cbb18df2e78f6fa0eb686835f50c30a38
Antoine Fournier <antoine.fournier14@hotmail.fr>
2015-10-29 20:56:37 +0100

    not finish

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,35 +1,41 @@
 class CocktailsController < ApplicationController
+ before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
 
-before_action :set_cocktail, only: [:show, :new]
-
+  # GET /cocktails
+  # GET /cocktails.json
   def index
     @cocktails = Cocktail.all
   end
 
+  # GET /cocktails/1
+  # GET /cocktails/1.json
   def show
-
   end
 
+  # GET /cocktails/new
   def new
     @cocktail = Cocktail.new
-
   end
 
+  # POST /cocktails
+  # POST /cocktails.json
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-    redirect_to cocktail_path(@cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
-
-  def cocktail_params
-    params.require(:cocktail).permit(:name)
-  end
-
 
   private
-   def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
 
-
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name)
+    end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,25 +1,31 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
 
-before_action :set_cocktail, only: [:create, :destroy]
+  def new
+    @dose = Dose.new
+  end
 
   def create
-
+    # @dose = Dose.new(dose_params)
+    # @dose.cocktail_id = @cocktail.id
+    # OR
+    @dose = @cocktail.doses.new(dose_params)
+    # then
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
-
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to :back
   end
 
   private
-
-  def set_cocktail
-    @cocktails = Cocktail.find(params[:id])
-  end
-
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
-
-
-
 end
