f27cb81b0f8f78fd9c057d96b4bec11237e8e835
Antoine Fournier <antoine.fournier14@hotmail.fr>
2015-10-29 14:24:54 +0100

    routes done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "Bacardi")
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
